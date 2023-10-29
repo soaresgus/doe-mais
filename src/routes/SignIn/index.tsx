f8647abee1b1259f52ea65091473b561d1e8a5ac
soaresgus <gustavosoaresbt@hotmail.com>
2023-10-28 22:16:56 -0300

    Feat - Cep: Adicionado integração com CEP para inputs de cadastro
    .

--- src/routes/SignIn/index.tsx
+++ src/routes/SignIn/index.tsx
@@ -1,6 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { Image, ScrollView, Text, View } from 'react-native';
 import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+} from 'react-native';
+import {
+  ActivityIndicator,
   Button,
   PaperProvider,
   RadioButton,
@@ -16,9 +24,48 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { Controller, useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { Link } from '@react-navigation/native';
+import cep from 'cep-promise';
+import { useMaskedInputProps } from 'react-native-mask-input';
+import states from '../../models/states.json';
+import ModalDropdown from 'react-native-modal-dropdown';
+
+const statesAcronyms = states.estados.map((item) => item.sigla);
+
+const dropdownRenderRow = (rowData: any, rowID: any, highlighted: any) => {
+  let evenRow = rowID % 2;
+  return (
+    <TouchableHighlight underlayColor="cornflowerblue">
+      <View
+        style={[
+          styles.dropdown_row,
+          { backgroundColor: evenRow ? 'lemonchiffon' : 'white' },
+        ]}
+      >
+        <Text
+          style={[
+            styles.dropdown_row_text,
+            highlighted && { color: 'mediumaquamarine' },
+          ]}
+        >
+          {`${rowData.name} (${rowData.age})`}
+        </Text>
+      </View>
+    </TouchableHighlight>
+  );
+};
+
+const dropdownRenderSeparator = (
+  sectionID: any,
+  rowID: any,
+  adjacentRowHighlighted: any
+) => {
+  let key = `spr_${rowID}`;
+  return <View style={styles.dropdown_separator} key={key} />;
+};
 
 const SignIn = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const [isLoadingCep, setIsLoadingCep] = useState(false);
 
   const createUserFormSchema = z.object({
     email: z
@@ -31,13 +78,18 @@ const SignIn = () => {
       .enum(['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'])
       .default('A+'),
     userType: z.enum(['doador', 'necessita']).default('doador'),
-    cep: z.string(),
+    street: z.string(),
+    neighborhood: z.string(),
+    city: z.string(),
+    number: z.number(),
   });
 
   const {
     control,
     handleSubmit,
     formState: { errors },
+    getValues,
+    setValue,
   } = useForm({
     resolver: zodResolver(createUserFormSchema),
     defaultValues: {
@@ -46,15 +98,52 @@ const SignIn = () => {
       email: '',
       bloodType: 'A+',
       userType: 'doador',
-      cep: '',
+      street: '',
+      neighborhood: '',
+      city: '',
+      number: '',
     },
   });
+
+  const [cepState, setCepState] = useState('');
+  const [state, setState] = useState('');
+
+  const appTheme = useTheme();
+
+  const fetchCep = async (cepValue: string) => {
+    setIsLoadingCep(true);
+    const result = await cep(cepValue);
+
+    setValue('street', result.street);
+    setValue('city', result.city);
+    setState(result.state);
+    setValue('neighborhood', result.neighborhood);
+
+    setIsLoadingCep(false);
+  };
+
+  const onCepInputChanged = async (cepValue: string) => {
+    setCepState(cepValue);
+    if (cepValue.length >= 9) {
+      await fetchCep(cepValue);
+    }
+  };
+
+  const cepMask = [/\d/, /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/];
+  const cepMaskedInputProps = useMaskedInputProps({
+    value: cepState,
+    onChangeText: onCepInputChanged,
+    mask: cepMask,
+  });
+
+  const dropdownRenderButtonText = (rowData: any) => {
+    const { name, age } = rowData;
+    return `${name} - ${age}`;
+  };
 
   const createUser = (data: any) => {
     console.log(data);
   };
-
-  const appTheme = useTheme();
 
   return (
     <ScrollView
@@ -89,6 +178,7 @@ const SignIn = () => {
             <Text className="text-lg text-red-500">{errors.name?.message}</Text>
           )}
         </View>
+
         <View>
           <Controller
             control={control}
@@ -112,6 +202,7 @@ const SignIn = () => {
             </Text>
           )}
         </View>
+
         <View>
           <Controller
             control={control}
@@ -274,6 +365,106 @@ const SignIn = () => {
           />
         </View>
 
+        <View className="flex-row mr-8 gap-4 items-center justify-center">
+          <Input label="CEP" extendedClasses="w-60" {...cepMaskedInputProps} />
+          {isLoadingCep && <ActivityIndicator />}
+        </View>
+
+        <View>
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <Input
+                label="Logradouro"
+                onChange={onChange}
+                onBlur={onBlur}
+                value={value}
+              />
+            )}
+            name="street"
+          />
+        </View>
+
+        <View>
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <Input
+                label="Número"
+                onChange={onChange}
+                onBlur={onBlur}
+                value={value}
+                keyboardType="number-pad"
+              />
+            )}
+            name="number"
+          />
+        </View>
+
+        <View>
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <Input
+                label="Bairro"
+                onChange={onChange}
+                onBlur={onBlur}
+                value={value}
+              />
+            )}
+            name="neighborhood"
+          />
+        </View>
+
+        <View>
+          <ModalDropdown
+            style={styles.dropdown}
+            textStyle={styles.dropdown_text}
+            dropdownStyle={styles.dropdown_dropdown}
+            options={statesAcronyms}
+            renderButtonText={(rowData: any) =>
+              dropdownRenderButtonText(rowData)
+            }
+            renderRow={(rowData: any, rowID: any, highlighted: any) =>
+              dropdownRenderRow(rowData, rowID, highlighted)
+            }
+            renderSeparator={(
+              sectionID: any,
+              rowID: any,
+              adjacentRowHighlighted: any
+            ) =>
+              dropdownRenderSeparator(sectionID, rowID, adjacentRowHighlighted)
+            }
+          />
+        </View>
+
+        <View>
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <Input
+                label="Cidade"
+                onChange={onChange}
+                onBlur={onBlur}
+                value={value}
+              />
+            )}
+            name="city"
+          />
+        </View>
+
         <Button
           className="w-80"
           mode="outlined"
@@ -292,5 +483,52 @@ const SignIn = () => {
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  dropdown: {
+    alignSelf: 'flex-end',
+    width: 150,
+    marginTop: 32,
+    right: 8,
+    borderWidth: 0,
+    borderRadius: 3,
+    backgroundColor: 'cornflowerblue',
+  },
+  dropdown_text: {
+    marginVertical: 10,
+    marginHorizontal: 6,
+    fontSize: 18,
+    color: 'white',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  },
+  dropdown_dropdown: {
+    width: 150,
+    height: 300,
+    borderColor: 'cornflowerblue',
+    borderWidth: 2,
+    borderRadius: 3,
+  },
+  dropdown_row: {
+    flexDirection: 'row',
+    height: 40,
+    alignItems: 'center',
+  },
+  dropdown_image: {
+    marginLeft: 4,
+    width: 30,
+    height: 30,
+  },
+  dropdown_row_text: {
+    marginHorizontal: 4,
+    fontSize: 16,
+    color: 'navy',
+    textAlignVertical: 'center',
+  },
+  dropdown_separator: {
+    height: 1,
+    backgroundColor: 'cornflowerblue',
+  },
+});
 
 export default SignIn;
